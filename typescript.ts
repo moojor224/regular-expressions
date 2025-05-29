6b0dbc96991f0382f8847f5707c1435d3eb150ae
moojor224 <moojor224@gmail.com>
2025-05-29 17:03:17 -0500

    Optimize autoHex regex

--- typescript.ts
+++ typescript.ts
@@ -3,7 +3,7 @@ export const uuid = /[a-zA-Z0-9]{8}(?:-[a-zA-Z0-9]{4}){3}-[a-zA-Z0-9]{12}/;
 /** matches hex RGB and RGBA values */
 export const hexa = /^#(?:(?:[0-9a-f]{3}){1,2}|(?:[0-9a-f]{4}){1,2})$/;
 /** matches hex RGB and RGBA values with name rgb and rgba groups */
-export const autoHex = /^#(?:(?<rgb>[a-fA-F0-9]{3}|[a-fA-F0-9]{6})|(?<rgba>[a-fA-F0-9]{4}|[a-fA-F0-9]{8}))$/;
+export const autoHex = /^#(?:(?<rgb>(?:[a-fA-F0-9]{3}){1,2})|(?<rgba>(?:[a-fA-F0-9]{4}){1,2}))$/;
 export const rgb = /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/;
 export const rgba = /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d?\.?\d+)\)$/;
 export const hsl = /^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/;
